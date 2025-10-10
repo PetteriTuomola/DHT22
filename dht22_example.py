64a0cba27126a772d298a16d3fdc870f61da1c9e
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-10 17:50:39 +0300

    Revert "Setup for my system."
    This reverts commit 57498b27ba518e67dada962a58b8243c1c25a3e8.

--- dht22_example.py
+++ dht22_example.py
@@ -5,21 +5,21 @@ import datetime
 
 # initialize GPIO
 GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-# read data using pin 7
-instance = dht22.DHT22(pin=7)
+# read data using pin 14
+instance = dht22.DHT22(pin=14)
 
 try:
 	while True:
-		result = instance.read()
-		if result.is_valid():
-			print("Last valid input: " + str(datetime.datetime.now()))
-			print("Temperature: %-3.1f C" % result.temperature)
-			print("Humidity: %-3.1f %%" % result.humidity)
-		else:
-			print("Non-valid input.")
-		time.sleep(1)
+	    result = instance.read()
+	    if result.is_valid():
+	        print("Last valid input: " + str(datetime.datetime.now()))
+
+	        print("Temperature: %-3.1f C" % result.temperature)
+	        print("Humidity: %-3.1f %%" % result.humidity)
+
+	    time.sleep(5)
 
 except KeyboardInterrupt:
     print("\nCleanup")
