68101fb1a122bc2b361948495288ac578b7d43cf
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-11 12:29:14 +0300

    More testing

--- c_dht22.cpp
+++ c_dht22.cpp
@@ -4,27 +4,29 @@
 
 static int edgecounter;
 
-static void cb_both() {
+static void cb_both(WPIWfiStatus status, void* userdata) {
     edgecounter++;
+    if (edgecounter % 10 == 0) {
+        std::cout << "ten" << std::endl;
+    }
 }
 
 
 int main() {
     int pin = 3; // GPIO pin number
-    edgecounter = 0;
     wiringPiSetupPinType(WPI_PIN_PHYS);
+    for (int i = 0; i < 100; i++) {
+        edgecounter = 0;
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, 1);
+        usleep(50000);
+        digitalWrite(pin, 0);
+        usleep(2000);
+        pinMode(pin, INPUT);
+        pullUpDnControl(pin, PUD_UP);
 
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, 1);
-    sleep(50000);
-    digitalWrite(pin, 0);
-    usleep(2000);
-    pinMode(pin, INPUT);
-    pullUpDnControl(pin, PUD_UP);
-
-    wiringPiISR(pin, INT_EDGE_BOTH, &cb_both);
-    std::cout << "Edge count: " << edgecounter << std::endl;
-    return 0;
+        wiringPiISR2(pin, INT_EDGE_BOTH, &cb_both);
+        std::cout << "Edge count: " << edgecounter << std::endl;
+        return 0;
+    }
 }
-
-main()
