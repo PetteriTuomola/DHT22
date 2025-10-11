b272a833aeb412a50b680ed62729d4fb1d95b3fc
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-11 14:14:48 +0300

    Should now give the actual values

--- c_dht22.cpp
+++ c_dht22.cpp
@@ -8,7 +8,7 @@ static int index;
 
 static void cb_both(WPIWfiStatus status, void* userdata) {
     edgecounter++;
-    timedata[index] = status.timeStamp_um;
+    timedata[index] = status.timeStamp_us;
     index++;
 }
 
@@ -26,12 +26,26 @@ int main() {
         pinMode(pin, INPUT);
         pullUpDnControl(pin, PUD_UP);
 
-        sleep(3);
-        for (int j = 0; j < 89; j++) {
-            std::cout << timedata[j + 1] - timedata[j] << ", ";
+        sleep(2);
+        int bytes[5] = {0, 0, 0, 0, 0};
+        int offset = 4
+        if (timedata[2] - timedata[1] < 40) offset++; // Sometimes it detects the first up, this corrects for that
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 8; j++) {
+                int time_diff = timedata[offset + j + i * 8 + 1] - timedata[offset + j + i * 8];
+                if (time_diff > 45) bytes[i] += pow(2, i);
+            }
         }
-        printf("\n");
+        if ((bytes[0] + bytes[1] + bytes[2] + bytes[3]) & 0xFF != bytes[4]) {
+            printf("Checksum error\n");
+            continue;
+        }
+        float humidity = ((bytes[0] * 256) + bytes[1]) * 0.1;
+        float temperature = ((bytes[2] * 256) + bytes[3]) * 0.1;
+        printf("Humidity: %.1f %%\n", humidity);
+        printf("Temperature: %.1f C\n", temperature);
         printf("Edge count: %d\n", edgecounter);
+        sleep(1);
     }
     return 0;
 }
