b57e2b7af30a6ae16d92349936107194db3a3b6a
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-11 13:02:24 +0300

    Not as good, more downs than ups

--- c_dht22.cpp
+++ c_dht22.cpp
@@ -17,7 +17,6 @@ int main() {
     int pin = 3; // GPIO pin number
     wiringPiSetupPinType(WPI_PIN_PHYS);
     wiringPiISR2(pin, INT_EDGE_BOTH, &cb_both, 0, NULL);
-    pullUpDnControl(pin, PUD_UP);
     for (int i = 0; i < 100; i++) {
         edgeupcounter = 0;
         edgedowncounter = 0;
@@ -25,9 +24,9 @@ int main() {
         digitalWrite(pin, 0);
         usleep(2000);
         pinMode(pin, INPUT);
+        pullUpDnControl(pin, PUD_UP);
 
         sleep(3);
-        printf(digitalRead(pin) ? "HIGH\n" : "LOW\n");
         printf("Rising edges: %d, Falling edges: %d\n", edgeupcounter, edgedowncounter);
     }
     return 0;
