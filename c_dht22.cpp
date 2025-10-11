93a73f45e618f3e33fa44be586a75aad7e60486d
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-11 12:12:12 +0300

    Relearning C

--- c_dht22.cpp
+++ c_dht22.cpp
@@ -1,22 +1,28 @@
 #include <wiringPi.h>
 #include <iostream>
+#include <unistd.h>
+
+static int edgecounter;
+
+statid void cb_both() {
+    edgecounter++;
+}
+
 
 int main() {
     int pin = 3; // GPIO pin number
-    int edgecounter = 0;
+    edgecounter = 0;
     wiringPiSetupPinType(WPI_PIN_PHYS);
 
     pinMode(pin, OUTPUT);
-    digitalwrite(pin, 1);
-    sleep(0.05);
+    digitalWrite(pin, 1);
+    sleep(50000);
     digitalWrite(pin, 0);
-    sleep(0.002);
+    usleep(2000);
     pinMode(pin, INPUT);
     pullUpDnControl(pin, PUD_UP);
 
-    wiringPiISR(pin, INT_EDGE_BOTH, []() {
-        edgecounter++;
-    });
+    wiringPiISR(pin, INT_EDGE_BOTH, &cb_both);
     std::cout << "Edge count: " << edgecounter << std::endl;
     return 0;
 }
