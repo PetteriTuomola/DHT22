356fdb518f917fd329e70d3668e7e67c9885bd41
PetteriTuomola <petteri.tuomola@gmail.com>
2025-10-11 11:57:45 +0300

    Swap to C++

---
+++ c_dht22.cpp
@@ -0,0 +1,23 @@
+#include <wiringPi.h>
+#include <iostream>
+
+int main() {
+    int pin = 3; // GPIO pin number
+    int edgecounter = 0;
+    wiringPiSetupPinType(WPI_PIN_PHYS);
+
+    pinMode(pin, OUTPUT);
+    digitalwrite(pin, 1);
+    sleep(0.05);
+    digitalWrite(pin, 0);
+    sleep(0.002);
+    pinMode(pin, INPUT);
+    pullUpDnControl(pin, PUD_UP);
+
+    wiringPiISR(pin, INT_EDGE_BOTH, []() {
+        edgecounter++;
+    });
+    sleep(2);
+    std::cout << "Edge count: " << edgecounter << std::endl;
+    return 0;
+}
